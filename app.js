60c2c968bc6787304766a0e480c736c0fda062f5
Mohd Irfan <mohdirfan70097@gmail.com>
2025-04-14 20:35:14 +0530

    Fresh Starting

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("movies/movie.ejs"); // Correct path
+});
+
+
+app.listen(8080,()=>{
+    console.log("Server sarted");
+    
+})
